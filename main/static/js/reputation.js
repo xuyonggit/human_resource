03d76286578c949668c1bcb72446efc9ab3f0a53
xuyong <xuyong_bj@163.com>
2018-08-21 17:55:48 +0800

    信用中心模块启动

--- main/static/js/reputation.js
+++ main/static/js/reputation.js
@@ -9,23 +9,28 @@ var TableInit2 = function () {
             url: "/biog/getFromUserInfo/",
             method: 'POST',
             dataType: 'json',
-            striped: true,
+            striped: false,
             border: "0px solid transparent",
             //showHeader: false,      //不显示列头
             //showFooter: true,       //是否显示列脚
             checkboxHeader: false,  // 设置false 将在列头隐藏check-all checkbox
             columns: [
-                {field: 'id', title: '排名', rowStyle: function (row, index) {
-                        if (index==1){
-                            return {css: {"background-color":"red"}}
-                        }else {
-                            return {css: {"background-color": "green"}}
-                        }
-                    }},
+                {field: 'id', title: '排名'},
                 {field: 'username', title: '用户名'},
                 {field: 'recommend_count', title: '推荐人数'},
                 {field: 'reputation', title: '麻豆信用分'},
-            ]
+            ],
+            rowStyle: function (row, index) {
+                        if(index==0){
+                            return {css:{"background-color": "#28ff28"}}
+                        }else if(index==1){
+                            return {css:{"background-color": "#feff7f"}}
+                        }else if(index==2){
+                            return {css:{"background-color": "#85e7ff"}}
+                        }else{
+                            return {css:{}}
+                        }
+            }
         })
     };
     return oTableInit2;
